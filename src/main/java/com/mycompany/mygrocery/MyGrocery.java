75565c2b08115be4ba33e795f7c8d98d7793f30f
AyatAlKhulaqi <89496383+AyatAlKhulaqi@users.noreply.github.com>
2023-05-31 11:19:40 +0800

    GitHub MyGrocery

---
+++ src/main/java/com/mycompany/mygrocery/MyGrocery.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mygrocery;
+
+/**
+ *
+ * @author Ayat
+ */
+public class MyGrocery {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
